daa2f0bd71a27ba756f9201397c9872286f06352
NimerLopez <nimerlopez1@gmail.com>
2023-02-27 14:13:11 -0600

    se cambia nombre de funcion

--- routes/NewRoutes.js
+++ routes/NewRoutes.js
@@ -41,7 +41,7 @@ route.post('/new', async (req, res) => {
       const results = await Promise.all(
         newssorce.map(async (item) => {//se recorren los recursos de la noticias
           const feed = await parser.parseURL(item.url);
-          const result = await saveRssDataToMongo(feed.items, item.user_id, item.category_id, item._id);
+          const result = await saveNewRSSDataToMongo(feed.items, item.user_id, item.category_id, item._id);
           return result;
         })
       );
@@ -55,7 +55,7 @@ route.post('/new', async (req, res) => {
   }
 });
 //contentSnippet
-async function saveRssDataToMongo(rssData, userid, categoriid, idnewsource) {
+async function saveNewRSSDataToMongo(rssData, userid, categoriid, idnewsource) {
   try {
     const results = [];
     for (const item of rssData) {
@@ -87,25 +87,25 @@ async function saveRssDataToMongo(rssData, userid, categoriid, idnewsource) {
 
 
 
-
-route.patch('/new/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { title, short_description, permalink, date, new_source_id, user_id, category_id } = req.body;
-    console.log(id);
-    let Id_user = await User.model.findById(req.body.user_id);//captura el usuario
-    console.log(Id_user);
-    if (Id_user) {//valida si existe el usuario
-      //let savedNew = await Newcon.updateOne({ _id:id},{$set:Newcon});//guarda datos
-      const savedNew = await New.findByIdAndUpdate(id, { title, short_description, permalink, date, new_source_id, user_id, category_id }, { new: true });
-      res.json(savedNew);
-    } else {
-      throw new Error('No se encontró el equipo');
-    }
-  } catch (error) {//se cae si no se encuentra 
-    res.json({ message: error.message + " No se encontro al equipo xx" });
-  }
-});
+//update new
+// route.patch('/new/:id', async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const { title, short_description, permalink, date, new_source_id, user_id, category_id } = req.body;
+//     console.log(id);
+//     let Id_user = await User.model.findById(req.body.user_id);//captura el usuario
+//     console.log(Id_user);
+//     if (Id_user) {//valida si existe el usuario
+//       //let savedNew = await Newcon.updateOne({ _id:id},{$set:Newcon});//guarda datos
+//       const savedNew = await New.findByIdAndUpdate(id, { title, short_description, permalink, date, new_source_id, user_id, category_id }, { new: true });
+//       res.json(savedNew);
+//     } else {
+//       throw new Error('No se encontró el equipo');
+//     }
+//   } catch (error) {//se cae si no se encuentra 
+//     res.json({ message: error.message + " No se encontro al equipo xx" });
+//   }
+// });
 //delete new 
 route.delete('/new/:id', (req, res) => {
   const { id } = req.params;
